9e8740aa594c6b91d28ace4dae221a4ec0b66be7
rishita <rishitagupta2700@gmail.com>
2024-02-29 16:51:11 -0500

    First Half of the app

---
+++ src/Components/HomeSection/TweetCard.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import { Avatar, Button, Menu, MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+
+const TweetCard = () => {
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDeleteTweet = () => {
+    console.log("delete tweet");
+    handleClose();
+  };
+
+  const handleOpenReplyModel = () => {
+    console.log("Open Model");
+  };
+
+  const handleCreateRetweets = () => {
+    console.log("handle create retweets");
+  };
+
+  const handleLikeTweet = () => {
+    console.log("handle like tweet");
+  };
+  return (
+    <div className="">
+      {/* <div className='flex items-center font-semibold text-gray-700 py-2'>
+            <RepeatIcon/>
+
+        </div> */}
+
+      <div className="flex space-x-5">
+        <Avatar
+          onClick={() => navigate(`/profile/${6}`)}
+          className="cursor-pointer"
+          alt="username"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmW78VpyB8SVmox3yBreQwV-hSh3Cc68Z6vQdaL02ojg&s"
+        />
+
+        <div className="w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex cursor-pointer items-center space-x-2">
+              <span className="font-semibold">Code with Zosh</span>
+              <span className="text-gray-600">@codewithzosh . 2m</span>
+              <img
+                className="ml-2 w-5 h-5"
+                src="https://img.freepik.com/premium-vector/verification-checkmark-blue-circle-star-vector-icon-isolated-white-background_261737-745.jpg?size=338&ext=jpg&ga=GA1.1.384202588.1708904478&semt=sph"
+                alt=""
+              />
+            </div>
+            <div>
+              <Button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <MoreHorizIcon />
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <MenuItem onClick={handleDeleteTweet}>Delete</MenuItem>
+                <MenuItem onClick={handleDeleteTweet}>Edit</MenuItem>
+              </Menu>
+            </div>
+          </div>
+
+          <div className="mt-2">
+            <div className="cursor-pointer">
+              <p className="mb-2 p-0">brutal but who cares</p>
+              <img
+                className="w-[21rem] border border-gray-400 p-5 rounded-md"
+                src="https://img.freepik.com/free-photo/beautiful-beach-sunrise-blue-sky_181624-26939.jpg?t=st=1709171991~exp=1709175591~hmac=5106f85186b8bed307666d8915e3aa76f35b777a707b4a0a66eda3e79b2a9cd3&w=360"
+                alt=""
+              />
+            </div>
+            <div className="py-5 flex flex-wrap justify-between items-center">
+              <div className="space-x-3 flex items-center text-gray-600">
+                <ChatBubbleOutlineIcon
+                  className="cursor-pointer"
+                  onClick={handleOpenReplyModel}
+                />
+                <p>43</p>
+              </div>
+
+              <div
+                className={`${
+                  true ? "text-gray-600" : "text-gray-600"
+                } space-x-3 flex items-center`}
+              >
+                <RepeatIcon
+                  className="cursor-pointer"
+                  onClick={handleCreateRetweets}
+                />
+                <p>54</p>
+              </div>
+
+              <div
+                className={`${
+                  true ? "text-pink-600" : "text-gray-600"
+                } space-x-3 flex items-center`}
+              >
+                {true ? (
+                  <FavoriteIcon
+                    className="cursor-pointer"
+                    onClick={handleLikeTweet}
+                  />
+                ) : (
+                  <FavoriteBorderIcon
+                    className="cursor-pointer"
+                    onClick={handleLikeTweet}
+                  />
+                )}
+                <p>54</p>
+              </div>
+              <div className="space-x-3 flex items-center text-gray-600">
+                <BarChartIcon
+                  className="cursor-pointer"
+                  onClick={handleOpenReplyModel}
+                />
+                <p>430</p>
+              </div>
+              <div className="space-x-3 flex items-center text-gray-600">
+                <FileUploadIcon
+                  className="cursor-pointer"
+                  onClick={handleOpenReplyModel}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TweetCard;
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { navigationMenu} from './NavigationMenu'
+import { useNavigate } from 'react-router-dom'
+import { Avatar, Button, Menu, MenuItem } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+
+const Navigation = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const navigate = useNavigate();
+  const handleLogout=()=>{
+    console.log("logout")
+    handleClose()
+  }
+  return (
+    <div className='h-screen sticky top-0'>
+      <div>
+        <div className='py-5'>
+          <svg height="30" width="30" viewBox="0 0 24 24" aria-hidden="true" class="r-jwli3a r-4qtqp9 r-yyyyoo r-labphf r-1777fci r-dnmrzs r-494qqr r-bnwqim r-lplcrui r-lrvibr"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.171-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5. 117z"></path></g></svg>
+        </div>
+        <div className='space-y-6'>
+          {navigationMenu.map((item) => <div className='cursor-pointer flex space-x-3 items-center' onClick={() => item.title === "Profile" ? navigate(`/profile/${5}`)
+            : navigate(item.path)}>
+            {item.icon}
+            <p className='text-xl'>{item.title} </p>
+          </div>)}
+        </div>
+
+        <div className='py-10'>
+          <Button sx={{
+            width: "100%",
+            borderRadius: "29px", py: "15px", bgcolor: "#1e88e5"
+          }} variant='contained'>
+            Tweet
+          </Button>
+
+        </div>
+      </div>
+
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center space-x-3'>
+
+          <Avatar
+            alt="username" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmW78VpyB8SVmox3yBreQwV-hSh3Cc68Z6vQdaL02ojg&s' />
+          <div>
+            <span>Rishita</span>
+            <span className='opacity-70'>@rishita.g</span>
+          </div>
+
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            <MoreHorizIcon />
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Navigation
